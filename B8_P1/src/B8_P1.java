dd49e518f7962e8819a12b2c864552d3e23c1cbe
opius2001 <lmongarman@gmail.com>
2021-01-19 14:31:01 +0100

    P1 en curso

--- B8_P1/src/B8_P1.java
+++ B8_P1/src/B8_P1.java
@@ -2,8 +2,13 @@
 public class B8_P1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Fecha oA = new Fecha((byte)16, (byte)04, (short)1984);
+		Fecha oB = new Fecha((byte)-5, (byte)6, (short)1984);
+		Fecha oC = new Fecha((byte)16, (byte)15, (short)1984);
+		
+		if (condition) {
+			
+		}
 	}
 
 }
--- B8_P1/src/Fecha.java
+++ B8_P1/src/Fecha.java
@@ -4,38 +4,86 @@ public class Fecha {
 	private byte bDia; //PK
 	private byte bMes; //PK
 	private short shAnio; //PK
-	private final byte BDIAMAX;
+	private final byte BDIAMAX = diaMaxFecha(getbMes(), esBisiesto(this.shAnio));
+	private final short SHANIOMAX = 9999;
 	
 	public Fecha(byte bDia, byte Mes, short shAnio) {
-		setbDia(bDia);
 		setbMes(bMes);
 		setShAnio(shAnio);
+		setbDia(bDia);
 	}
 	
 	public byte getbDia() {
 		return bDia;
 	}
 	public void setbDia(byte bDia) {
-		this.bDia = bDia;
+		if (bDia > 0 && bDia <= BDIAMAX ) {
+			this.bDia = bDia;
+		} else {
+			this.bDia = -1;
+		}
 	}
 	public byte getbMes() {
 		return bMes;
 	}
 	public void setbMes(byte bMes) {
-		this.bMes = bMes;
+		if (bMes > 0 && bMes <= 12 ) {
+			this.bMes = bMes;
+		} else {
+			this.bMes = -1;
+		}
+		
 	}
 	public short getShAnio() {
 		return shAnio;
 	}
 	public void setShAnio(short shAnio) {
-		if (shAnio > 1 && shAnio < 9999) {
+		if (shAnio > 1 && shAnio < SHANIOMAX) {
 			this.shAnio = shAnio;
+		} else {
+			this.shAnio = -1;
 		}
 	}
 	
-	private byte diaMax(short shAnio, byte bMes) {
-		byte bDiaMax = 0;
+	private static byte diaMaxFecha(byte bMes, boolean booBisiesto) {
+		byte bDiaMax;
+		
+		if (bMes==2) {
+			if (booBisiesto) {
+				bDiaMax = 29;
+			} else {
+				bDiaMax = 28;
+			}
+		} else if (bMes==4 || bMes==6 || bMes==9 || bMes==11) {
+			bDiaMax = 30;
+		} else {
+			bDiaMax = 31;
+		}
 		
 		return bDiaMax;
 	}
+	
+	private static boolean esBisiesto(short shAnio) {
+		boolean booEsBisiesto;
+
+		if ((shAnio % 4 == 0 && shAnio % 100 != 0) || shAnio % 400 == 0) {
+			booEsBisiesto = true;
+		} else {
+			booEsBisiesto = false;
+		}
+
+		return booEsBisiesto;
+	}
+	
+	public boolean checkFecha() {
+		boolean booExito = false;
+		
+		if (this.getbDia() > 0 && this.getbMes() > 0 && this.getShAnio() > 0) {
+			booExito = true;
+		}
+		return booExito;
+	}
+	
+	
+	
 }

2116b049411dc6306198793d138e25065d6b7329
opius2001 <lmongarman@gmail.com>
2021-01-19 20:12:47 +0100

    P2 acabado

---
+++ B8_P2/src/B8_P2.java
@@ -0,0 +1,22 @@
+
+public class B8_P2 {
+
+	public static void main(String[] args) {
+		Punto oPunto1 = new Punto();
+		Punto oPunto2 = new Punto(0.5 , 2.4);
+		double dDistancia;
+
+		oPunto2.borrar();
+		System.out.println(oPunto2.toString());
+		oPunto1.mover(0.8, 2.9);
+		oPunto2.mover(oPunto1);
+		System.out.println("Despues del cambio "+oPunto2.toString());
+
+		oPunto1.mover(0.8, 0.4);
+		System.out.println("La distnacia euclidia entre el punto 1 y el punto 2 es "+oPunto1.distanciaEuclidiana(oPunto2));
+		
+		
+		
+	}
+
+}
---
+++ B8_P2/src/Punto.java
@@ -0,0 +1,74 @@
+
+public class Punto {
+
+	private double dX;
+	private double dY;
+
+	public Punto() {
+		this.dX = 0f;
+		this.dY = 0f;
+	}
+
+	public Punto(double dX, double dY) {
+		setdX(dX);
+		setdY(dY);
+	}
+
+	/**
+	 * @return the dX
+	 */
+	public double getdX() {
+		return dX;
+	}
+
+	/**
+	 * @param dX the dX to set
+	 */
+	public void setdX(double dX) {
+		this.dX = dX;
+	}
+
+	/**
+	 * @return the dY
+	 */
+	public double getdY() {
+		return dY;
+	}
+
+	/**
+	 * @param dY the dY to set
+	 */
+	public void setdY(double dY) {
+		this.dY = dY;
+	}
+
+	public void borrar() {
+		this.dX = 0f;
+		this.dY = 0f;
+	}
+
+	public void mover(double dX, double dY) {
+		setdX(dX);
+		setdY(dY);
+	}
+
+	public void mover(Punto oPunto) {
+		setdX(oPunto.getdX());
+		setdY(oPunto.getdY());
+	}
+
+	public double distanciaEuclidiana(Punto oPunto) {
+		double dDistancia;
+
+		dDistancia = Math.sqrt(Math.pow((oPunto.getdX() - this.dX), 2) + Math.pow((oPunto.getdY() - this.dY), 2));
+
+		return dDistancia;
+	}
+
+	public String toString() {
+		String sMensaje;
+		sMensaje = "(" + getdX() + " , " + getdY() + ")";
+		return sMensaje;
+	}
+
+}

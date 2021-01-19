73ef58c19dd7c933eef16d71d57f5b4f08e4c21c
opius2001 <lmongarman@gmail.com>
2021-01-19 23:38:46 +0100

    Probando git en eclipse

--- B8_P4/src/B8_P4.java
+++ B8_P4/src/B8_P4.java
@@ -2,7 +2,7 @@
 public class B8_P4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		MaquinaExpendedora oMaquina1 = new MaquinaExpendedora((short) 11245);
 
 	}
 

731631a91528cfecb849f850ad6ac06c160ff8bd
opius2001 <lmongarman@gmail.com>
2021-01-19 23:34:13 +0100

    P4 a medias

---
+++ B8_P4/src/B8_P4.java
@@ -0,0 +1,9 @@
+
+public class B8_P4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ B8_P5/src/ProgramadorBombillas.java
@@ -0,0 +1,52 @@
+
+public class ProgramadorBombillas {
+
+	 Bombilla bombilla1;
+	 Bombilla bombilla2;
+	 Bombilla bombilla3;
+	 Bombilla bombilla4;
+	 Bombilla bombilla5;
+	 Bombilla bombilla6;
+	 
+	 
+	 
+	public Bombilla getBombilla1() {
+		return bombilla1;
+	}
+	public void setBombilla1(Bombilla bombilla1) {
+		this.bombilla1 = bombilla1;
+	}
+	public Bombilla getBombilla2() {
+		return bombilla2;
+	}
+	public void setBombilla2(Bombilla bombilla2) {
+		this.bombilla2 = bombilla2;
+	}
+	public Bombilla getBombilla3() {
+		return bombilla3;
+	}
+	public void setBombilla3(Bombilla bombilla3) {
+		this.bombilla3 = bombilla3;
+	}
+	public Bombilla getBombilla4() {
+		return bombilla4;
+	}
+	public void setBombilla4(Bombilla bombilla4) {
+		this.bombilla4 = bombilla4;
+	}
+	public Bombilla getBombilla5() {
+		return bombilla5;
+	}
+	public void setBombilla5(Bombilla bombilla5) {
+		this.bombilla5 = bombilla5;
+	}
+	public Bombilla getBombilla6() {
+		return bombilla6;
+	}
+	public void setBombilla6(Bombilla bombilla6) {
+		this.bombilla6 = bombilla6;
+	}
+	 
+	 
+	 
+}

d066acb0e43b7149a0a7ed016612cc3863d5ea18
opius2001 <lmongarman@gmail.com>
2021-01-21 21:18:14 +0100

    Peq mejorar P5

--- B8_P5/src/B8_P5.java
+++ B8_P5/src/B8_P5.java
@@ -4,7 +4,7 @@ public class B8_P5 {
 	public static void main(String[] args) {
 		ProgramadorBombillas oBombillasSalon = new ProgramadorBombillas((byte) 6);
 		
-		oBombillasSalon.programa1();
+		oBombillasSalon.programa1(); 
 		oBombillasSalon.programa2();
 		oBombillasSalon.programa3();
 		
--- B8_P5/src/ProgramadorBombillas.java
+++ B8_P5/src/ProgramadorBombillas.java
@@ -40,11 +40,11 @@ public class ProgramadorBombillas {
 				aBombillas[bContador].apagar();
 			}
 		}
-		System.out.println(mostrarBombillas(aBombillas));
+		System.out.println(toString());
 		for (byte bContador = 0; bContador < this.aBombillas.length; bContador++) {
 			aBombillas[bContador].cambiar();
 		}
-		System.out.println(mostrarBombillas(aBombillas));
+		System.out.println(toString());
 		System.out.println("------------");
 		apagarTodo();
 	}
@@ -59,7 +59,7 @@ public class ProgramadorBombillas {
 				aBombillas[bContador - 1].apagar();
 			} 
 			aBombillas[bContador].encender();
-			System.out.println(mostrarBombillas(aBombillas));
+			System.out.println(toString());
 		}
 		System.out.println("------------");
 		apagarTodo();
@@ -80,7 +80,7 @@ public class ProgramadorBombillas {
 		for (byte bVeces = 0; bVeces < 2; bVeces++) {
 			for (byte bContador = (byte) (this.aBombillas.length - 1); bContador >=  0 ; bContador--) {
 				aBombillas[bContador].cambiar();
-				System.out.println(mostrarBombillas(aBombillas));
+				System.out.println(toString());
 			}
 		}
 		System.out.println("------------");
@@ -91,16 +91,6 @@ public class ProgramadorBombillas {
 		
 		for (int bContador = 0; bContador < this.aBombillas.length; bContador++) {
 			sMostrar += this.aBombillas[bContador].mostrar() + " ";
-		}
-		
-		return sMostrar;
-	}
-	
-	public String mostrarBombillas(Bombilla[] aBombillas) {
-		String sMostrar = "";
-		
-		for (int bContador = 0; bContador < aBombillas.length; bContador++) {
-			sMostrar += aBombillas[bContador].mostrar() + " ";
 		}
 		
 		return sMostrar;
@@ -121,57 +111,9 @@ public class ProgramadorBombillas {
 	private void apagarTodo() {
 		for (byte bContador = 0; bContador < this.aBombillas.length; bContador++) {
 			if (aBombillas[bContador].isBooEncendida()) {
-				aBombillas[bContador].apagar();
+				aBombillas[bContador].cambiar();
 			}
 		}
-	}
-	
-//	 Bombilla bombilla1;
-//	 Bombilla bombilla2;
-//	 Bombilla bombilla3;
-//	 Bombilla bombilla4;
-//	 Bombilla bombilla5;
-//	 Bombilla bombilla6;
-//	 
-//	 
-//	 
-//	public Bombilla getBombilla1() {
-//		return bombilla1;
-//	}
-//	public void setBombilla1(Bombilla bombilla1) {
-//		this.bombilla1 = bombilla1;
-//	}
-//	public Bombilla getBombilla2() {
-//		return bombilla2;
-//	}
-//	public void setBombilla2(Bombilla bombilla2) {
-//		this.bombilla2 = bombilla2;
-//	}
-//	public Bombilla getBombilla3() {
-//		return bombilla3;
-//	}
-//	public void setBombilla3(Bombilla bombilla3) {
-//		this.bombilla3 = bombilla3;
-//	}
-//	public Bombilla getBombilla4() {
-//		return bombilla4;
-//	}
-//	public void setBombilla4(Bombilla bombilla4) {
-//		this.bombilla4 = bombilla4;
-//	}
-//	public Bombilla getBombilla5() {
-//		return bombilla5;
-//	}
-//	public void setBombilla5(Bombilla bombilla5) {
-//		this.bombilla5 = bombilla5;
-//	}
-//	public Bombilla getBombilla6() {
-//		return bombilla6;
-//	}
-//	public void setBombilla6(Bombilla bombilla6) {
-//		this.bombilla6 = bombilla6;
-//	}
-	 
-	 
+	}	 
 	 
 }

588348dcfed68c43cf567d48a6741a13e0e0fb7e
opius2001 <lmongarman@gmail.com>
2021-01-25 11:00:39 +0100

    Recosntruccion ACT9

---
+++ ACT9_E1/src/ACT9_E1.java
@@ -0,0 +1,11 @@
+
+public class ACT9_E1 {
+
+	public static void main(String[] args) {
+		Producto p = new Producto("Cola-Cao",-10.5F, 3);
+		System.out.println("El precio del producto " + p.getsNombre() + " es de " + p.getfPrecio() + " Euros");
+		System.out.println("Hay " + p.getiUnidades() + " en stock, valoradas es " + p.valorEnStock() + " Euros");
+
+	}
+
+}

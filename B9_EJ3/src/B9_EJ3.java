86539932f2ce6087aacbbbf93865fa9b43585b08
opius2001 <lmongarman@gmail.com>
2021-01-27 15:07:12 +0100

    SEGURIDAD

--- B9_EJ3/src/B9_EJ3.java
+++ B9_EJ3/src/B9_EJ3.java
@@ -2,8 +2,8 @@
 public class B9_EJ3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
--- B9_EJ3/src/Numero.java
+++ B9_EJ3/src/Numero.java
@@ -1,4 +1,52 @@
 
 public class Numero {
-
+	private double dNumero;
+	private int iNumero;
+	private long lNumero;
+	private float fNumero;
+	//instance of 
+	public Numero(double dNumero) {
+		setdNumero(dNumero);
+	}
+	public Numero(int iNumero) {
+		double dNumero = iNumero;
+		setdNumero(dNumero);
+	}
+	public Numero(long lNumero) {
+		double dNumero = lNumero;
+		setdNumero(dNumero);
+	}
+	public Numero(float fNumero) {
+		double dNumero = fNumero;
+		setdNumero(dNumero);
+	}
+	
+	
+	public double getdNumero() {
+		return dNumero;
+	}
+	public void setdNumero(double dNumero) {
+		this.dNumero = dNumero;
+	}
+	public int getiNumero() {
+		return iNumero;
+	}
+	public void setiNumero(int iNumero) {
+		this.iNumero = iNumero;
+	}
+	public long getlNumero() {
+		return lNumero;
+	}
+	public void setlNumero(long lNumero) {
+		this.lNumero = lNumero;
+	}
+	public float getfNumero() {
+		return fNumero;
+	}
+	public void setfNumero(float fNumero) {
+		this.fNumero = fNumero;
+	}
+	
+	
 }
+

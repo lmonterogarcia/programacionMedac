6bf1de39155c2f9e33528fb961b6ed50660e9d35
opius2001 <lmongarman@gmail.com>
2021-01-29 21:23:22 +0100

    P4 clase articulo creada y terminada

--- B9_P4/src/Articulo.java
+++ B9_P4/src/Articulo.java
@@ -1,4 +1,54 @@
 
 public class Articulo {
 
+	private int iId; //PK
+	private String sNombre; // NN
+	private double dPrecio; // NN
+	
+	public Articulo(int iId) {
+		getiId();
+	}
+	
+	public Articulo(int iId, String sNombre, double dPrecio) {
+		getiId();
+		getsNombre();
+		getdPrecio();
+	}
+	
+	public int getiId() {
+		return iId;
+	}
+	public boolean setiId(int iId) {
+		boolean booExito = false;
+		if (iId > 0 && iId < Integer.MAX_VALUE) {
+			this.iId = iId;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public String getsNombre() {
+		return sNombre;
+	}
+	public boolean setsNombre(String sNombre) {
+		boolean booExito = false;
+		if (sNombre != null && sNombre.length() > 5) {
+			this.sNombre = sNombre;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public double getdPrecio() {
+		
+		return dPrecio;
+	}
+	public boolean setdPrecio(double dPrecio) {
+		boolean booExito = false;
+		if (dPrecio > 0 && dPrecio < Double.MAX_VALUE) {
+			this.dPrecio = dPrecio;
+			booExito = true;
+		}
+		return booExito;
+		
+	}
+	
 }

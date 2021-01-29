9c845c93c974ae63634ec23c3df992434bff44ea
opius2001 <lmongarman@gmail.com>
2021-01-29 14:05:40 +0100

    EJ Terminado

--- B9_EJ3/src/B9_EJ3.java
+++ B9_EJ3/src/B9_EJ3.java
@@ -2,7 +2,17 @@
 public class B9_EJ3 {
 
 	public static void main(String[] args) {
-		Numero iNum = new Numero((int) 3);
+		Numero oNum = new Numero((int) 3);
+		
+		oNum.sumar((int)5);
+		System.out.println(oNum);
+		oNum.restar((double)2.2567867895954679425454456456676756875645645687554687593);
+		System.out.println(oNum);
+		oNum.multiplicar((float)1.78948592);
+		System.out.println(oNum);
+		oNum.dividir((long)2);
+		System.out.println(oNum);
+
 		
 	}
 

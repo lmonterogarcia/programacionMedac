9d8432cf56e6bc4275d0ca6732a22b7a7738ab35
opius2001 <lmongarman@gmail.com>
2021-02-01 13:06:20 +0100

    inicio boletin

--- TIENDA_DAM/src/models/ICliente.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public interface ICliente {
-	public String getsTarjeta();
-	public void setsTarjeta(String sTarjeta);
-	public String toString();
-	public boolean checkCliente();
-}
--- TIENDA_DAM/src/models/IEmpleado.java
+++
@@ -1,8 +0,0 @@
-package models;
-
-public interface IEmpleado {
-	public String getsSegSocial();
-	public void setsSegSocial(String sSegSocial);
-	public String toString();
-	public boolean checkEmpleado();
-}

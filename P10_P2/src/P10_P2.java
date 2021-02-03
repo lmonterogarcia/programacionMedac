2cf4815d095f1192f2ee4eead5e23af65e5adf7b
Luis <Montero>
2021-02-03 11:37:15 +0100

    Mejoras P2 main

--- P10_P2/src/P10_P2.java
+++ P10_P2/src/P10_P2.java
@@ -2,7 +2,12 @@
 public class P10_P2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ITelefono oTel;
+		Satelite oSat = new Satelite(658847562);
+		Movil oMov = new Movil(632598741);
+		Fijo oFij = new Fijo(684597542);
+		
+		
 
 	}
 

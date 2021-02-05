0a13d2f33b4c8422f7300a37a4544231735d409c
Luis <Montero>
2021-02-05 10:32:48 +0100

    Creacion de todas las interfaces y clases. Creacion de variables Get and set. creacion de metodos en Ipublicacion

--- B10_P3/src/IPublicacion.java
+++ B10_P3/src/IPublicacion.java
@@ -1,4 +1,16 @@
+import java.util.Calendar;
 
 public interface IPublicacion {
 
+	public int getIcodigo();
+	public boolean setIcodigo(int iCodigo);
+	public String getsAutor();
+	public boolean setsAutor(String sAutor);
+	public String getsTirulo();
+	public boolean setsTirulo(String sTitulo);
+	public short getShAnnoPublicacion();
+	public boolean setShAnnoPublicacion(short shAnnoPublicacion);
+	public boolean isBooPrestado();
+	public void setBooPrestado(boolean booPrestado);
+	
 }
--- B10_P3/src/Revista.java
+++ B10_P3/src/Revista.java
@@ -1,4 +1,62 @@
+import java.util.Calendar;
 
-public class Revista {
-
+public class Revista implements IPublicacion{
+	private int iCodigo;
+	private String sAutor;
+	private String sTitulo;
+	private short shAnnoPublicacion;
+	private boolean booPrestado;
+	
+	
+	public int getIcodigo() {
+		return iCodigo;
+	}
+	public boolean setIcodigo(int iCodigo) {
+		boolean booExito = false;
+		if (iCodigo >= 100000 && iCodigo <= 999999) {
+			this.iCodigo = iCodigo;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public String getsAutor() {
+		return sAutor;
+	}
+	public boolean setsAutor(String sAutor) {
+		boolean booExito = false;
+		if (sAutor != null && sAutor.length() <= 255) {
+			this.sAutor = sAutor;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public String getsTirulo() {
+		return sTitulo;
+	}
+	public boolean setsTirulo(String sTitulo) {
+		boolean booExito = false;
+		if (sTitulo != null && sTitulo.length() <= 255) {
+			this.sTitulo = sTitulo;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public short getShAnnoPublicacion() {
+		return shAnnoPublicacion;
+	}
+	public boolean setShAnnoPublicacion(short shAnnoPublicacion) {
+		boolean booExito = false;
+		Calendar.getInstance();
+		if (shAnnoPublicacion >= 1440 && shAnnoPublicacion <= (short) Calendar.getInstance().YEAR) {
+			this.shAnnoPublicacion = shAnnoPublicacion;
+			booExito = true;
+		}
+		return booExito;
+	}
+	public boolean isBooPrestado() {
+		return booPrestado;
+	}
+	public void setBooPrestado(boolean booPrestado) {
+		this.booPrestado = booPrestado;
+	}
 }

aaf65b6bf4b94a08bd17d5d7cda1aa958c07424a
Luis Montero <lmongarman@gmail.com>
2021-02-15 22:06:46 +0100

    EJ1 Terminado

---
+++ B11_EJ1/B11_EJ1.java
@@ -0,0 +1,10 @@
+/**
+ * B11_EJ1
+ */
+public class B11_EJ1 {
+
+    public static void main(String[] args) {
+        ReproductorPortatil oR = new ReproductorPortatil("Sony", "NM-WM1Z", (byte) 3);
+        System.out.println(oR);
+    }
+}
--- B11_EJ1/IReproductorPortatil.java
+++ B11_EJ1/IReproductorPortatil.java
@@ -13,29 +13,42 @@ public interface IReproductorPortatil {
 
     public String getsMarca();
     public boolean setsMarca(String sMarca);
+
     public String getsModelo();
     public boolean setsModelo(String sModelo);
+
     public boolean isBooReproduceSonido();
     public boolean setBooReproduceSonido(boolean booReproduceSonido);
+   
     public boolean isBooReproduceVideo();
     public boolean setBooReproduceVideo(boolean booReproduceVideo);
+    
     public byte getbTipoDeAlmacenamiento();
     public boolean setbTipoDeAlmacenamiento(byte bTipoDeAlmacenamiento);
+    
     public byte getbPantalla();
     public boolean setbPantalla(byte bPantalla);
+    
     public byte getbTipoDeBateria();
     public boolean setbTipoDeBateria(byte bTipoDeBateria);
+    
     public byte getbAutonomia();
     public boolean setbAutonomia(byte bAutonomia);
-    public short getShCapacidadDeAlmacenamiento();
-    public boolean setShCapacidadDeAlmacenamiento(short shCapacidadDeAlmacenamiento);
+    
+    public int getiCapacidadDeAlmacenamiento();
+    public boolean setiCapacidadDeAlmacenamiento(short shCapacidadDeAlmacenamiento);
+    
     public float getfPeso();
     public boolean setfPeso(float fPeso);
+    
     public float getfAncho();
     public boolean setfAncho(float fAncho);
+    
     public float getfAlto();
     public boolean setfAlto(float fAlto);
+    
     public float getfGrosor();
     public boolean setfGrosor(float fGrosor);
+    
     public String toString();
 }

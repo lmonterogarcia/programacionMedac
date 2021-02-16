dc8a850b1eb376bae27db902bc9c6580e8f2e0d7
Luis Montero <lmongarman@gmail.com>
2021-02-16 22:30:04 +0100

    P2 termiando

---
+++ B11_P2/B11_P2.java
@@ -0,0 +1,20 @@
+public class B11_P2 {
+    public static void main(String[] args) {
+        ReproductorMultimedia oRm = new ReproductorMultimedia("Sony", "NM-WM1Z", (byte) 1, true, true, false, false, (byte) 21);
+        
+        oRm.setbAutonomia((byte) 20);
+        oRm.setbTipoDeAlmacenamiento((byte) 3);
+        oRm.setBooReproduceVideo(true);
+        oRm.setbPantalla((byte) 3);
+        oRm.setbTipoDeBateria((byte) 2);
+        oRm.setiCapacidadDeAlmacenamiento(20000);
+
+        oRm.setbooRadio(true);
+        oRm.setbooGrabacionDeVoz(true);
+
+        oRm.setbooReproduceDIVX(true);
+        oRm.setbooTelevision(true);
+
+        System.out.println(oRm);
+    }
+}

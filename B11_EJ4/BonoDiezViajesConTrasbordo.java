11f8acdf86c8ad17a90fef9c79a219e7cc6ffa74
Luis Montero <lmongarman@gmail.com>
2021-02-17 23:56:17 +0100

    Bono DiezViajesConTrasbordo termiando

--- B11_EJ4/BonoDiezViajesConTrasbordo.java
+++ B11_EJ4/BonoDiezViajesConTrasbordo.java
@@ -3,6 +3,7 @@ import java.util.Calendar;
 public class BonoDiezViajesConTrasbordo extends BonoDiezViajes {
    
     private Calendar oUltimoViaje;
+    private final int IUNAHORA = 3600000;
 
     public BonoDiezViajesConTrasbordo(int idBonoBus) {
         super(idBonoBus);
@@ -12,15 +13,10 @@ public class BonoDiezViajesConTrasbordo extends BonoDiezViajes {
         boolean booExito = false;
         
         if (transbordo(shLineaBus, bDia, bMes, shAnio, bHora, bMinuto)){
-            booExito = true;
-            super.setShLineaBus(shLineaBus);
-            super.setbDia(bDia);
-            super.setbMes(bMes);
-            super.setShAnio(shAnio);
-            super.setbHoras(bHora);
-            super.setbMinutos(bMinuto);
-            oUltimoViaje.set(shAnio, bMes, bDia, bHora, bMinuto);
-
+            if (super.setShLineaBus(shLineaBus) && super.setbDia(bDia) && super.setbMes(bMes) && super.setShAnio(shAnio) && super.setbHoras(bHora) && super.setbMinutos(bMinuto)) {
+                booExito = true;
+                //oUltimoViaje.set(shAnio, bMes, bDia, bHora, bMinuto); // Si se quiere que cada vez que haga trasbordo empice de nuevo el tiempo, descomentar esta linea. 
+            }
         } else if (super.booValido) {
             if (super.setShLineaBus(shLineaBus) && super.setbDia(bDia) && super.setbMes(bMes) && super.setShAnio(shAnio) && super.setbHoras(bHora) && super.setbMinutos(bMinuto)) {
                 setbViajesRealizados((byte) (this.bViajesRealizados + 1));
@@ -38,7 +34,7 @@ public class BonoDiezViajesConTrasbordo extends BonoDiezViajes {
         oFechaViaje.set(shAnio, bMes, bDia, bHora, bMinuto);
 
         if (super.getbViajesRealizados() > 0 && super.getbViajesRealizados() >= 10 && shLineaBus != super.getShLineaBus()) {
-            if (oUltimoViaje.compareTo(oFechaViaje) >= 0 && oUltimoViaje.compareTo(oFechaViaje) <= 3600000) {
+            if (oUltimoViaje.compareTo(oFechaViaje) >= 0 && oUltimoViaje.compareTo(oFechaViaje) <= IUNAHORA) {
                 booExito = true;
             }
         }

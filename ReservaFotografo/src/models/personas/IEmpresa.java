254465c238a3fb17f0f2a0fdfb711877801a58cc
Luis <Montero>
2021-04-02 09:19:25 +0200

    Interfaz de EMpresa creado

--- ReservaFotografo/src/models/personas/IEmpresa.java
+++ ReservaFotografo/src/models/personas/IEmpresa.java
@@ -1,5 +1,22 @@
 package models.personas;
 
+import models.lugar.Lugar;
+
 public interface IEmpresa {
     
+    public String getsCifNif();
+    public void setsCifNif(String sCifNif);
+    public String getsNombreEmpresa();
+    public void setsNombreEmpresa(String sNombreEmpresa);
+    public Lugar getoLugar();
+    public void setoLugar(Lugar oLugar);
+    public String getsEmailEmpresa();
+    public void setsEmailEmpresa(String sEmailEmpresa);
+    public String getsTelefonoEmrpesa();
+    public void setsTelefonoEmrpesa(String sTelefonoEmrpesa);
+    
+    public boolean checkEmpresa();
+    public int hashCode();
+    public boolean equals(Object obj);
+    public String toString();
 }

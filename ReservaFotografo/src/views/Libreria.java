4a054148fd6e98dce624dfc681748e7e7290d5ec
Luis <Montero>
2021-04-12 14:23:05 +0200

    mejoras y empezar view y ctrl

---
+++ ReservaFotografo/src/views/Libreria.java
@@ -0,0 +1,141 @@
+package views;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Libreria {
+    public static Object leer(String sMensaje, long lMinimo, long lMaximo, double dMinimo, double dMaximo, byte bEstado) {
+		Object oNumero;
+		switch(bEstado) {
+		case 1:
+			oNumero = pideNumeroByte(sMensaje,lMinimo,lMaximo);
+			break;
+		case 2:
+			oNumero = pideNumeroShort(sMensaje,lMinimo,lMaximo);
+			break;
+		case 3:
+			oNumero = pideNumeroInt(sMensaje,lMinimo,lMaximo);
+			break;
+		case 4:
+			oNumero = pideNumeroLong(sMensaje,lMinimo,lMaximo);
+			break;
+		case 5:
+			oNumero = pideNumeroFloat(sMensaje,dMinimo,dMaximo);
+			break;
+		case 6:
+			oNumero = pideString(sMensaje,lMinimo,lMaximo);
+			break;
+		default:
+			oNumero = -1;
+		}
+		return oNumero;
+	}
+
+	private static byte pideNumeroByte(String sMensaje, long lMinimo, long lMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		byte bNumero = 0;
+		boolean bFallo;
+
+		do {
+			System.out.print(sMensaje + "("+lMinimo+" - " +lMaximo+"): ");
+			try {
+				bNumero = Byte.parseByte(teclado.readLine());
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (bNumero < lMinimo || bNumero > lMaximo));
+
+		return bNumero;
+	}
+	
+	private static short pideNumeroShort(String sMensaje, long lMinimo, long lMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		short sNumero = 0;
+		boolean bFallo;
+		
+		do {
+			System.out.print(sMensaje + "("+lMinimo+" - " +lMaximo+"): ");
+			try {
+				sNumero = Short.parseShort(teclado.readLine());
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (sNumero < lMinimo || sNumero > lMaximo));
+
+		return sNumero;
+	}
+	
+	private static int pideNumeroInt(String sMensaje, long lMinimo, long lMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		int iNumero = 0;
+		boolean bFallo;
+
+		do {
+			System.out.print(sMensaje + "("+lMinimo+" - " +lMaximo+"): ");
+			try {
+				iNumero = Integer.parseInt(teclado.readLine());
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (iNumero < lMinimo || iNumero > lMaximo));
+
+		return iNumero;
+	}
+	
+	private static long pideNumeroLong(String sMensaje, long lMinimo, long lMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		long lNumero = 0;
+		boolean bFallo;
+
+		do {
+			System.out.print(sMensaje + "("+lMinimo+" - " +lMaximo+"): ");
+			try {
+				lNumero = Long.parseLong(teclado.readLine());
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (lNumero < lMinimo || lNumero > lMaximo));
+
+		return lNumero;
+	}
+	
+	private static float pideNumeroFloat(String sMensaje, double dMinimo, double dMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		float fNumero = 0;
+		boolean bFallo;
+
+		do {
+			System.out.print(sMensaje + "("+dMinimo+" - " +dMaximo+"): ");
+			try {
+				fNumero = Float.parseFloat(teclado.readLine());
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (fNumero < dMinimo || fNumero > dMaximo));
+
+		return fNumero;
+	}
+	
+	private static String pideString(String sMensaje, long lMinimo, long lMaximo) {
+		BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+		String sFrase = "";
+		boolean bFallo;
+
+		do {
+			System.out.print(sMensaje+": ");
+			try {
+				sFrase = teclado.readLine();
+				bFallo = false;
+			} catch (Exception e) {
+				bFallo = true;
+			}
+		} while (bFallo || (sFrase.length() < lMinimo || sFrase.length() > lMaximo));
+
+		return sFrase;
+	}
+}
--- ReservaFotografo/src/views/ReservaFotografo.java
+++ ReservaFotografo/src/views/ReservaFotografo.java
@@ -3,93 +3,123 @@ package views;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import controllers.Controller;
 import models.lugar.*;
 import models.sesion.*;
 import models.productos.*;
 import models.personas.*;
 
 public class ReservaFotografo {
-    public static void main(String[] args) {
-        Provincia oProvincia = new Provincia("Sevilla");
-        Pais oPais = new Pais("Espania", (short) 123, "ES");
-        PaisProvincia oPaisProvincia = new PaisProvincia(oProvincia, oPais);
-        Localidad oLocalidad = new Localidad("Sevilla");
-        CodigoPostal oCodigoPostal = new CodigoPostal("41007");
-        CodigoPostalLocalidadPaisProvincia oCodigoPostalLocalidadPaisProvincia = new CodigoPostalLocalidadPaisProvincia(
-                oLocalidad, oCodigoPostal, oPaisProvincia);
-        Lugar oLugar = new Lugar("Prueba",
-                "https://www.google.es/maps/place/MEDAC+Sevilla+Este+-+Formación+Profesional/@37.4122534,-5.9596897,15z/data=!4m8!1m2!2m1!1smedac+sevilla!3m4!1s0x0:0xe78091d604579c0c!8m2!3d37.4064864!4d-5.9330383",
-                "Calle Falsa", "12B", 37.406630f, -5.932992f, oCodigoPostalLocalidadPaisProvincia);
-        Usuario oUsuario = new Usuario("elmio@gmail.com", "qweqwe");
-        Cliente oCliente = new Cliente("28818261C", "luis", "Montero", "Garcia-Manjaron", "625478547",
-                LocalDate.of(1984, 04, 24), oUsuario, oLugar);
-        Participante oParticipante = new Participante("24578746R", "Miguel", "Alacid", "Mierder", "654785142", LocalDate.of(1990,05,12), "juarjuar@gmail.com");
-        Fotografo oFotografo = new Fotografo("25486514T", "IreneFoto");
-        Empresa oEmpresa = new Empresa("41574865E", "Empresa Falsa", "empresa@gmail.com", "954875447", oLugar);
-        TipoSesion oTipoSesion = new TipoSesion("Comunion", (short)240);
-        Estado oEstado = new Estado("Presesion");
-        EstadoTipoSesion oEstadoTipoSesion = new EstadoTipoSesion(oEstado, oTipoSesion, (byte) 1);
-        Producto oProducto = new Producto("Copias 10x15", 25f, "Mate y con borde");
-        Pack oPack = new Pack("Sesion Comunion", 250f, "2 horas de sesion mas iglesia");
-        PackProducto oPackProducto = new PackProducto(oPack, oProducto);
-        Pedido oPedido = new Pedido(false, oEmpresa);
-        Sesion oSesion = new Sesion(874, oCliente, oPedido, LocalDate.of(2021, 4, 12), LocalTime.of(18, 00), oLugar, oEstadoTipoSesion);
-        PackSesion oPackSesion = new PackSesion(oPack, oSesion);
-        ProductoSesion oProductoSesion = new ProductoSesion(oSesion, oProducto);
-        FotografoSesion oFotografoSesion = new FotografoSesion(oFotografo, oSesion);
-        ParticipanteSesion oParticipanteSesion = new ParticipanteSesion(oParticipante, oSesion);
+        public static void main(String[] args) throws Exception {
+                /* TESTEO de las clases
+                Provincia oProvincia = new Provincia("Sevilla");
+                Pais oPais = new Pais("Espania", (short) 123, "ES");
+                PaisProvincia oPaisProvincia = new PaisProvincia(oProvincia, oPais);
+                Localidad oLocalidad = new Localidad("Sevilla");
+                CodigoPostal oCodigoPostal = new CodigoPostal("41007");
+                CodigoPostalLocalidadPaisProvincia oCodigoPostalLocalidadPaisProvincia = new CodigoPostalLocalidadPaisProvincia(
+                        oLocalidad, oCodigoPostal, oPaisProvincia);
+                Lugar oLugar = new Lugar("Prueba",
+                        "https://www.google.es/maps/place/MEDAC+Sevilla+Este+-+Formación+Profesional/@37.4122534,-5.9596897,15z/data=!4m8!1m2!2m1!1smedac+sevilla!3m4!1s0x0:0xe78091d604579c0c!8m2!3d37.4064864!4d-5.9330383",
+                        "Calle Falsa", "12B", 37.406630f, -5.932992f, oCodigoPostalLocalidadPaisProvincia);
+                Usuario oUsuario = new Usuario("elmio@gmail.com", "qweqwe");
+                Cliente oCliente = new Cliente("28818261C", "luis", "Montero", "Garcia-Manjaron", "625478547",
+                        LocalDate.of(1984, 04, 24), oUsuario, oLugar);
+                Participante oParticipante = new Participante("24578746R", "Miguel", "Alacid", "Mierder", "654785142", LocalDate.of(1990,05,12), "juarjuar@gmail.com");
+                Fotografo oFotografo = new Fotografo("25486514T", "IreneFoto");
+                Empresa oEmpresa = new Empresa("41574865E", "Empresa Falsa", "empresa@gmail.com", "954875447", oLugar);
+                TipoSesion oTipoSesion = new TipoSesion("Comunion", (short)240);
+                Estado oEstado = new Estado("Presesion");
+                EstadoTipoSesion oEstadoTipoSesion = new EstadoTipoSesion(oEstado, oTipoSesion, (byte) 1);
+                Producto oProducto = new Producto("Copias 10x15", 25f, "Mate y con borde");
+                Pack oPack = new Pack("Sesion Comunion", 250f, "2 horas de sesion mas iglesia");
+                PackProducto oPackProducto = new PackProducto(oPack, oProducto);
+                Pedido oPedido = new Pedido(false, oEmpresa);
+                Sesion oSesion = new Sesion(874, oCliente, oPedido, LocalDate.of(2021, 4, 12), LocalTime.of(18, 00), oLugar, oEstadoTipoSesion);
+                PackSesion oPackSesion = new PackSesion(oPack, oSesion);
+                ProductoSesion oProductoSesion = new ProductoSesion(oSesion, oProducto);
+                FotografoSesion oFotografoSesion = new FotografoSesion(oFotografo, oSesion);
+                ParticipanteSesion oParticipanteSesion = new ParticipanteSesion(oParticipante, oSesion);
 
-        System.out.println(oProvincia);
-        System.out.println("\n#######################################################");
-        System.out.println(oPais);
-        System.out.println("\n#######################################################");
-        System.out.println(oPaisProvincia);
-        System.out.println("\n#######################################################");
-        System.out.println(oLocalidad);
-        System.out.println("\n#######################################################");
-        System.out.println(oCodigoPostal);
-        System.out.println("\n#######################################################");
-        System.out.println(oCodigoPostalLocalidadPaisProvincia);
-        System.out.println("\n#######################################################");
-        System.out.println(oLugar);
-        System.out.println("\n#######################################################");
-        System.out.println(oUsuario);
-        System.out.println("\n#######################################################");
-        System.out.println(oCliente);
-        System.out.println("\n#######################################################");
-        System.out.println(oParticipante);
-        System.out.println("\n#######################################################");
-        System.out.println(oFotografo);
-        System.out.println("\n#######################################################");
-        System.out.println(oEmpresa);
-        System.out.println("\n#######################################################");
-        System.out.println(oTipoSesion);
-        System.out.println("\n#######################################################");
-        System.out.println(oEstado);
-        System.out.println("\n#######################################################");
-        System.out.println(oEstadoTipoSesion);
-        System.out.println("\n#######################################################");
-        System.out.println(oProducto);
-        System.out.println("\n#######################################################");
-        System.out.println(oPack);
-        System.out.println("\n#######################################################");
-        System.out.println(oPackProducto);
-        System.out.println("\n#######################################################");
-        System.out.println(oPedido);
-        System.out.println("\n#######################################################");
-        System.out.println(oSesion);
-        System.out.println("\n#######################################################");
-        
-        // Estos objeton no tienen toString!!!!!!!!!
-        System.out.println(oPackSesion);
-        System.out.println("\n#######################################################");
-        System.out.println(oProductoSesion);
-        System.out.println("\n#######################################################");
-        System.out.println(oFotografoSesion);
-        System.out.println("\n#######################################################");
-        System.out.println(oParticipanteSesion);
-        
+                System.out.println(oProvincia);
+                System.out.println("\n#######################################################");
+                System.out.println(oPais);
+                System.out.println("\n#######################################################");
+                System.out.println(oPaisProvincia);
+                System.out.println("\n#######################################################");
+                System.out.println(oLocalidad);
+                System.out.println("\n#######################################################");
+                System.out.println(oCodigoPostal);
+                System.out.println("\n#######################################################");
+                System.out.println(oCodigoPostalLocalidadPaisProvincia);
+                System.out.println("\n#######################################################");
+                System.out.println(oLugar);
+                System.out.println("\n#######################################################");
+                System.out.println(oUsuario);
+                System.out.println("\n#######################################################");
+                System.out.println(oCliente);
+                System.out.println("\n#######################################################");
+                System.out.println(oParticipante);
+                System.out.println("\n#######################################################");
+                System.out.println(oFotografo);
+                System.out.println("\n#######################################################");
+                System.out.println(oEmpresa);
+                System.out.println("\n#######################################################");
+                System.out.println(oTipoSesion);
+                System.out.println("\n#######################################################");
+                System.out.println(oEstado);
+                System.out.println("\n#######################################################");
+                System.out.println(oEstadoTipoSesion);
+                System.out.println("\n#######################################################");
+                System.out.println(oProducto);
+                System.out.println("\n#######################################################");
+                System.out.println(oPack);
+                System.out.println("\n#######################################################");
+                System.out.println(oPackProducto);
+                System.out.println("\n#######################################################");
+                System.out.println(oPedido);
+                System.out.println("\n#######################################################");
+                System.out.println(oSesion);
+                System.out.println("\n#######################################################");
+                
+                // Estos objeton no tienen toString!!!!!!!!!
+                System.out.println(oPackSesion);
+                System.out.println("\n#######################################################");
+                System.out.println(oProductoSesion);
+                System.out.println("\n#######################################################");
+                System.out.println(oFotografoSesion);
+                System.out.println("\n#######################################################");
+                System.out.println(oParticipanteSesion);
+                */
+                
+                byte bOpcion;
+                Controller oCtrl = new Controller();
 
+                if (oCtrl.init()) {
+                        do {
+				bOpcion = menu();
 
-    }
+				switch (bOpcion) {
+				case 1: // Gestion de clientes
+					ClienteView.gestionClientes(oCtrl);
+					break;
+				default:
+					System.out.println("Hasta luego.");
+				}
+
+			} while (bOpcion != 2);
+			oCtrl.closeDb();
+                } else {
+                        System.out.println("Error al conectar con la base de datos.");
+                }
+        }
+
+        public static byte menu() {
+		System.out.println("##################################");
+		System.out.println("Bienvenido a la APP de Reserva Fotografos");
+		System.out.println("##################################");
+		System.out.println("1. Gestion de clientes");
+		System.out.println("2. Salir");
+		return (byte) Libreria.leer("Introduce una opcion", 1, 2, -1, -1, (byte) 1);
+	}
 }
